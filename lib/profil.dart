681eeb17d5a3b5564a390ed640490f459d83463f
mekouar1999 <othman.mekouar@etu.univ-cotedazur.fr>
2023-03-16 12:30:00 +0100

    profil pret , next step , send info firebase a collection user

--- lib/profil.dart
+++ lib/profil.dart
@@ -20,6 +20,7 @@ class _ProfilScreenState extends State<ProfilScreen> {
   late TextEditingController _villeController;
 
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
 
   @override
@@ -36,6 +37,15 @@ class _ProfilScreenState extends State<ProfilScreen> {
 
   }
 
+  void _enregistrerModifications() {
+  // Implémentez la logique d'enregistrement des données ici
+
+  // Affichez un message de confirmation
+  ScaffoldMessenger.of(context).showSnackBar(
+    const SnackBar(content: Text('Modifications enregistrées')),
+  );
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,140 +54,190 @@ class _ProfilScreenState extends State<ProfilScreen> {
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 16.0, right: 16.0,bottom: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            
-
-            const SizedBox(height: 16.0),
-            TextFormField(
-              controller: _nomController,
-              decoration: const InputDecoration(
-                labelText: 'Nom',
-              ),
-            ),
-
-            const SizedBox(height: 16.0),
-
+       body: Padding(
+        padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 20),
+        child: Form( // Ajout du widget Form ici
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              
           
-            TextFormField(
-              controller: _prenomController,
-              decoration: const InputDecoration(
-                labelText: 'Prénom',
-              ),
+              const SizedBox(height: 16.0),
+          
+                TextFormField(
+                  controller: _nomController,
+                  decoration: const InputDecoration(
+                    labelText: 'Nom',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez entrer votre nom';
+                    }
+                    return null;
+                  },
+                ),
+          
+              const SizedBox(height: 16.0),
+          
+              TextFormField(
+                  controller: _prenomController,
+                  decoration: const InputDecoration(
+                    labelText: 'Prénom',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez entrer votre prénom';
+                    }
+                    return null;
+                  },
+                ),
+          
+              const SizedBox(height: 16.0),
+          
+              TextFormField(
+                  controller: _villeController,
+                  decoration: const InputDecoration(
+                    labelText: 'Ville',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez entrer votre ville';
+                    }
+                    return null;
+                  },
+                ),
+          
+               const SizedBox(height: 16.0),
+          
+              TextFormField(
+                  controller: _loginController,
+                  decoration: const InputDecoration(
+                    labelText: 'Login',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez entrer votre login';
+                    }
+                    return null;
+                  },
+                ),
+          
+              const SizedBox(height: 16.0),
+          
+              TextFormField(
+                  controller: _codePostaleController,
+                  decoration: const InputDecoration(
+                    labelText: 'Code Postal',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez entrer votre code postal';
+                    }
+                    return null;
+                  },
+                ),
+          
+              const SizedBox(height: 16.0),
+          
+              TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez saisir votre email';
+                    }
+                    return null;
+                  },
+                ),
+          
+              const SizedBox(height: 16.0),
+          
+              TextFormField(
+                  controller: _motDePasseController,
+                  decoration: const InputDecoration(
+                    labelText: 'mot de passe ',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez saisir votre mot de passe ';
+                    }
+                    return null;
+                  },
+                ),
+          
+              const SizedBox(height: 16.0),
+          
+                  TextFormField(
+                  controller: _dateNaissanceController,
+                  decoration: const InputDecoration(
+                    labelText: 'Date de naissance ',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez saisir votre date de naissance ';
+                    }
+                    return null;
+                  },
+                ),
+          
+              const SizedBox(height: 36.0),
+          
+          
+          
+                         
+          
+                  Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+            if (_formKey.currentState!.validate()) {
+              // Enregistrer les modifications
+          
+                    _enregistrerModifications(); // Appelez la fonction ici
+          
+            }
+                  },
+                  style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.yellow,
+            minimumSize: const Size(150, 50),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
             ),
-
-            const SizedBox(height: 16.0),
-
-            TextFormField(
-              controller: _villeController,
-              decoration: const InputDecoration(
-                labelText: 'Ville',
-              ),
+                  ),
+                  child: const Text('Enregistrer'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const LoginScreen()),
+            );
+                  },
+                  style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.yellow,
+            minimumSize: const Size(200, 50),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
             ),
-
-             const SizedBox(height: 16.0),
-
-            TextFormField(
-              controller: _loginController,
-              decoration: const InputDecoration(
-                labelText: 'Login',
-              ),
+                  ),
+                  child: const Text('Se déconnecter'),
+                ),
+              ],
             ),
-
-            const SizedBox(height: 16.0),
-
-            TextFormField(
-              controller: _codePostaleController,
-              decoration: const InputDecoration(
-                labelText: 'Code Postal',
-              ),
-            ),
-
-            const SizedBox(height: 16.0),
-
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-              ),
-            ),
-
-            const SizedBox(height: 16.0),
-
-            TextFormField(
-              controller: _motDePasseController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Mot de passe',
-              ),
-            ),
-
-            const SizedBox(height: 16.0),
-
-            TextFormField(
-              controller: _dateNaissanceController,
-              decoration: const InputDecoration(
-                labelText: 'Date de naissance',
-              ),
-            ),
-
-            const SizedBox(height: 36.0),
-
-
-           Center(
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-
-
-
-      ElevatedButton(
-        onPressed: () {
-          // Enregistrer les modifications
-        },
-        style: ElevatedButton.styleFrom(
-          foregroundColor: Colors.black, backgroundColor: Colors.yellow,
-          minimumSize: const Size(150, 50),
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
           ),
-        ),
-        child: const Text('Enregistrer'),
+            ],
+                  ),
+          ),
       ),
-
-
-
-     ElevatedButton(
-  onPressed: () {
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => const LoginScreen()),
-    );
-  },
-  style: ElevatedButton.styleFrom(
-    foregroundColor: Colors.black,
-    backgroundColor: Colors.yellow,
-    minimumSize: const Size(200, 50),
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.all(Radius.circular(10)),
     ),
-  ),
-  child: const Text('Se déconnecter'),
-),
-
-
-
-
-    ],
-  ),
-),
-
-          ],
-        ),
-      ),
     );
   }
 
